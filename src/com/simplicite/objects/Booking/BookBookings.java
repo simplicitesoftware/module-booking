10a0ba02c80329793ced6eea5b1edb7786fc9fee
designer (Designer) <no-reply@simplicite.fr>
2021-06-28 10:42:05 +0200

    Updated

--- src/com/simplicite/objects/Booking/BookBookings.java
+++ src/com/simplicite/objects/Booking/BookBookings.java
@@ -9,4 +9,12 @@ import com.simplicite.util.tools.*;
  */
 public class BookBookings extends ObjectDB {
 	private static final long serialVersionUID = 1L;
+	
+	@Override
+	public List<String> postValidate() {
+		List<String> msgs = new ArrayList<>();
+		if (Tool.diffDate(getFieldValue("bookBookbookingsStartDate"), getFieldValue("bookBookbookingsEndDate")) <=0)
+				msgs.add(Message.formatError("BOOK_ERROR_DATE", null, "bookBookbookingsEndDate"));
+		return msgs;
+	}
 }
