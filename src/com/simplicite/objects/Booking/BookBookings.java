8eb1b82f86e3d26176cc2ef3e3835501fa73b2cd
designer (Designer) <no-reply@simplicite.fr>
2021-06-28 10:22:05 +0200

    Updated

---
+++ src/com/simplicite/objects/Booking/BookBookings.java
@@ -0,0 +1,12 @@
+package com.simplicite.objects.Booking;
+
+import java.util.*;
+import com.simplicite.util.*;
+import com.simplicite.util.tools.*;
+
+/**
+ * Business object BookBookings
+ */
+public class BookBookings extends ObjectDB {
+	private static final long serialVersionUID = 1L;
+}
